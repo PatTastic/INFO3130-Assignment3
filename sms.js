2d707c97a76ba0cdb7909142aa5b40bb19a47efa
Pat Wilken <patrickwilken28@gmail.com>
2018-10-09 21:33:39 -0400

    Added content type header

--- sms.js
+++ sms.js
@@ -3,7 +3,9 @@ import timeoutPromise from './timeout-promise';
 const SMS = {
     sendSMS: function(res, msg, end){
         return new Promise((resolve, reject) => {
-            //res.writeHead(200, {'Content-Type': 'text/xml'});
+            if(!res.headersSent){
+                res.writeHead(200, {'Content-Type': 'text/xml'});
+            }
 
             msg = '<Response><Message>' + msg + '</Message></Response>';
             (end ? res.end(msg) : res.write(msg));
