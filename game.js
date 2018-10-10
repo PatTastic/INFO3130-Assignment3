ee7173e67cfbd6fa763afca5b9de28316eb10029
Pat Wilken <patrickwilken28@gmail.com>
2018-10-09 22:24:58 -0400

    Now saving flags per user
    Now gathering messages into one response

--- game.js
+++ game.js
@@ -20,15 +20,15 @@ const Game = {
             localStorage = new LocalStorage('./scratch');
         }
 
-        let savedPlayer = localStorage.getItem('player');
+        let savedPlayer = localStorage.getItem(from + '_player');
         if(!Utils.doesExist(savedPlayer) || savedPlayer.length == 0){
             API.checkIfNumberExists(from).then((player) => {
-                localStorage.setItem('player', JSON.stringify(player));
+                localStorage.setItem(from + '_player', JSON.stringify(player));
                 Game.setupGame(req, res, body, from, player);
             }).catch(() => {
                 API.createPlayer('', from).then(() => {
                     API.checkIfNumberExists(from).then((player) => {
-                        localStorage.setItem('player', JSON.stringify(player));
+                        localStorage.setItem(from + '_player', JSON.stringify(player));
                         Game.setupGame(req, res, body, from, player);
                     }).catch((err) => {
                         console.log('ERROR: ' + err);
@@ -43,10 +43,10 @@ const Game = {
         }
     },
     setupGame: function(req, res, body, from, player){
-        localStorage.setItem('progress', player.storyProgress);
+        localStorage.setItem(from + '_progress', player.storyProgress);
         API.updatePlayerProgress(from, player.storyProgress);
         API.isChoice(player.storyProgress).then((isChoice) => {
-            localStorage.setItem('isChoice', (isChoice == 1 ? 'true' : 'false'));
+            localStorage.setItem(from + '_isChoice', (isChoice == 1 ? 'true' : 'false'));
         }).catch((err) => {
             console.log('ERROR: ' + err);
         })
@@ -54,9 +54,9 @@ const Game = {
         Game.determineIfChoice(req, res, body, from);
     },
     determineIfChoice: function(req, res, body, from){
-        let progress = localStorage.getItem('progress');
+        let progress = localStorage.getItem(from + '_progress');
 
-        if(localStorage.getItem('isChoice') == 'true'){
+        if(localStorage.getItem(from + '_isChoice') == 'true'){
             Game.getChoices(req, res, body, from);
         }
         else{
@@ -64,8 +64,7 @@ const Game = {
         }
     },
     getChoices: function(req, res, body, from){
-        console.log('1');
-        API.getChoices(localStorage.getItem('progress')).then((choices) => {
+        API.getChoices(localStorage.getItem(from + '_progress')).then((choices) => {
             let toStory = 0;
 
             for(let i=0; i<choices.length; i++){
@@ -98,13 +97,13 @@ const Game = {
     getNextStory: function(req, res, body, from, toStory){
         API.getNextStory(toStory).then((toSend) => {
             if(toSend.isChoice){
-                localStorage.setItem('isChoice', 'true');
+                localStorage.setItem(from + '_isChoice', 'true');
                 Game.determineIfSMS(req, res, body, from, toSend);
                 //Game.getNextChoices(req, res, body, from, toStory);
             }
             else{
-                localStorage.setItem('progress', toSend.toNextStory);
-                localStorage.setItem('isChoice', 'false');
+                localStorage.setItem(from + '_progress', toSend.toNextStory);
+                localStorage.setItem(from + '_isChoice', 'false');
                 API.updatePlayerProgress(from, toSend.toNextStory);
                 Game.determineIfSMS(req, res, body, from, toSend);
             }
@@ -113,7 +112,6 @@ const Game = {
         })
     },
     getNextChoices: function(req, res, body, from, fromStory){
-        console.log('2');
         API.getChoices(fromStory).then((toSend) => {
             Game.determineIfSMS(req, res, body, from, toSend);
         }).catch((err) => {
@@ -122,10 +120,9 @@ const Game = {
     },
     determineIfSMS: function(req, res, body, from, toSend){
         let end = (toSend.isChoice);
-        let smsTimeout = TimeoutPromise(toSend.sendDelay, SMS.sendSMS(res, toSend.body, end));
+        let smsTimeout = TimeoutPromise(toSend.sendDelay, SMS.sendSMS(res, from, toSend.body, end));
 
         smsTimeout.then(() => {
-            console.log()
             if(!toSend.isChoice){
                 Game.play(req, res, body, from);
             }
--- sms.js
+++ sms.js
@@ -1,17 +1,45 @@
 import timeoutPromise from './timeout-promise';
+import nodeLocalstorage from 'node-localstorage';
+import utils from './utils';
+
+const Utils = utils.Utils;
+const LocalStorage = nodeLocalstorage.LocalStorage;
+var localStorage;
 
 const SMS = {
-    sendSMS: function(res, msg, end){
+    sendSMS: function(res, from, msg, end){
         return new Promise((resolve, reject) => {
-            if(!res.headersSent){
-                res.writeHead(200, {'Content-Type': 'text/xml'});
+            if (typeof localStorage === 'undefined' || localStorage === null) {
+                localStorage = new LocalStorage('./scratch');
             }
 
-            msg = '<Response><Message>' + msg + '</Message></Response>';
-            (end ? res.end(msg) : res.write(msg));
+            let wholeMsg = localStorage.getItem(from + '_sms');
+            if(!Utils.doesExist(wholeMsg) || wholeMsg.length == 0){
+                wholeMsg = '';
+            }
+
+            wholeMsg += '<Message>' + msg + '</Message>';
+            localStorage.setItem(from + '_sms', wholeMsg);
+
+            if(end){
+                wholeMsg = SMS.parseBody(wholeMsg);
+                localStorage.removeItem(from + '_sms');
+
+                res.writeHead(200, {'Content-Type': 'text/xml'});
+                res.end('<Response>' + wholeMsg + '</Response>');
+            }
 
             resolve(true);
         })
+    },
+    parseBody: function(msg){
+        let player = localStorage.getItem(from + '_player');
+        player = JSON.parse(player);
+
+        msg = msg.replace(/\%name\%/gi, player.name);
+        msg = msg.replace(/\\n/gi, '%0a');
+
+        return msg;
     }
 };
 
